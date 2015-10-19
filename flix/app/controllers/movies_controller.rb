49e5fc046313f54f9fe9b0bc0f5e75589621f6c6
anne <richardson.ae@gmail.com>
2015-10-19 14:42:48 -0500

    added show method

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -4,5 +4,10 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     #@movies = Movie.limit(1)
   end #index
+
+  def show
+    @movie = Movie.find(params[:id]) 
+  end #show
+    
     
 end #MoviesController
