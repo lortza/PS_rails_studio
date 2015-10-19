dd9d43581afda70837db398ef08873c4083ceddc
anne <richardson.ae@gmail.com>
2015-10-19 15:18:32 -0500

    added link_tos from index page to show page

--- events/config/routes.rb
+++ events/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "events" => "events#index"
-  get "events/:id" => "events#show"
+  get "events/:id" => "events#show", as: "event"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
