f0604f22f85c647e2d9420718cad40bfadab0bd9
anne <richardson.ae@gmail.com>
2015-10-19 16:08:33 -0500

    added as: movie to routes and then lnked to each movie show page via the movie title

--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   #verb "url" => "controller#action"
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
