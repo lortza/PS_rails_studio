0df0054b10a56153c04741de49b6771db6ac120a
anne <richardson.ae@gmail.com>
2015-10-19 14:36:09 -0500

    added show via a view, routes, and the controller

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -5,5 +5,11 @@ class EventsController < ApplicationController
     #@events = Event.limit(1)
     #@events = ["Kitty Watch", "Porch Swinging", "Back-Porch Sittin", "Big Night Out"] 
   end #index
+
+  def show
+    @event = Event.find(params[:id])
+
+  end #show
+    
     
 end #EventsController
