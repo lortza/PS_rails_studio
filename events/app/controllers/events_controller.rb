2fe5ada3d057cb1680003d1259d143be16ad1c2d
anne <richardson.ae@gmail.com>
2015-10-21 15:54:33 -0500

    added functionality for update button

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -14,7 +14,14 @@ class EventsController < ApplicationController
   def edit
     @event = Event.find(params[:id]) 
   end #edit
-    
+
+  def update
+    @event = Event.find(params[:id]) 
+    event_params = params.require(:event).
+                    permit(:name, :description, :location, :price, :starts_at)
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end #update
     
     
 end #EventsController
