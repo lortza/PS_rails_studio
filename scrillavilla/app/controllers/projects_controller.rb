3f23a800155fd62a078679be5a6df3f68d354956
anne <richardson.ae@gmail.com>
2015-10-21 17:05:13 -0500

    gave params to the projects

--- scrillavilla/app/controllers/projects_controller.rb
+++ scrillavilla/app/controllers/projects_controller.rb
@@ -71,6 +71,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params[:project]
+      params.require(:project).permit(:name, :description, :target_pledge_amount, :website, :pledging_ends_on)
     end
 end
