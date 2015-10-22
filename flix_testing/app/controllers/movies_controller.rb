b75cd21b2a499d7e554318de98c983804fd5bbcb
anne <richardson.ae@gmail.com>
2015-10-22 14:11:15 -0500

    created new spec for show page

--- flix_testing/app/controllers/movies_controller.rb
+++ flix_testing/app/controllers/movies_controller.rb
@@ -2,5 +2,10 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end #index
+
+  def show
+    @movie = Movie.find(params[:id]) 
+  end #show
+    
     
 end #MoviesController
--- flix_testing/config/routes.rb
+++ flix_testing/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
