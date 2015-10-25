c8e0ac8eea5d95f7e039e306326170a22f3831ad
anne <richardson.ae@gmail.com>
2015-10-25 16:00:48 -0500

    seeded database

--- fundraising_testing/db/seeds.rb
+++ fundraising_testing/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Project.create!([
+  {
+    name: "Personalized Robot Suits like Iron Man",
+    website: "www.website.com",
+    target_pledge_amount: 318412101.00,
+    description: "Tony Stark builds an armored suit to fight the throes of evil",
+    pledging_ends_on: "2008-05-02"
+  },
+  {
+    name: "Personal Flying Machine",
+    website: "www.website.com",
+    target_pledge_amount: 134218018.00,
+    description: "Clark Kent grows up to be the greatest super-hero",
+    pledging_ends_on: "1978-12-15"
+  },
+  {
+    name: "Wall-Walking Gloves and Boots",
+    website: "www.website.com",
+    target_pledge_amount: 403706375.00,
+    description: "Peter Parker gets bit by a genetically modified spider",
+    pledging_ends_on: "2002-05-03"
+  },
+  {
+    name: "Invisibility Cloak",
+    website: "www.website.com",
+    target_pledge_amount: 40200000.00,
+    description: "Patience Philips has a more than respectable career as a graphic designer",
+    pledging_ends_on: "2004-07-23"
+  }
+])
