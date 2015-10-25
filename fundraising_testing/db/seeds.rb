fb37188d9e6190c209fb974fd23f1b8e52aff4e4
anne <richardson.ae@gmail.com>
2015-10-25 16:14:40 -0500

    updated seeds

--- fundraising_testing/db/seeds.rb
+++ fundraising_testing/db/seeds.rb
@@ -14,27 +14,27 @@ Project.create!([
     website: "www.website.com",
     target_pledge_amount: 318412101.00,
     description: "Tony Stark builds an armored suit to fight the throes of evil",
-    pledging_ends_on: "2008-05-02"
+    pledging_ends_on: 10.days.from_now
   },
   {
     name: "Personal Flying Machine",
     website: "www.website.com",
     target_pledge_amount: 134218018.00,
     description: "Clark Kent grows up to be the greatest super-hero",
-    pledging_ends_on: "1978-12-15"
+    pledging_ends_on: 1.day.from_now
   },
   {
     name: "Wall-Walking Gloves and Boots",
     website: "www.website.com",
     target_pledge_amount: 403706375.00,
     description: "Peter Parker gets bit by a genetically modified spider",
-    pledging_ends_on: "2002-05-03"
+    pledging_ends_on: 45.days.from_now
   },
   {
     name: "Invisibility Cloak",
     website: "www.website.com",
     target_pledge_amount: 40200000.00,
     description: "Patience Philips has a more than respectable career as a graphic designer",
-    pledging_ends_on: "2004-07-23"
+    pledging_ends_on: 1.day.ago
   }
 ])
