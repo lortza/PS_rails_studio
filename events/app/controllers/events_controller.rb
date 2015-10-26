240b4e1a0d0092c1532a2192efd56100dd406a32
anne <richardson.ae@gmail.com>
2015-10-26 14:13:32 -0500

    created actions for form update

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -17,10 +17,11 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id]) 
-    event_params = params.require(:event).
-                    permit(:name, :description, :location, :price, :starts_at)
+    event_params = params
+                  .require(:event)
+                  .permit(:name, :description, :location, :price, :starts_at)
     @event.update(event_params)
-    redirect_to event_path(@event)
+    redirect_to event_path(@event) # or redirect_to @event
   end #update
     
     
--- events/config/routes.rb
+++ events/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  #verb "url pattern" => "controller#method", as: linkhelpername
   get "events" => "events#index"
   get "events/:id" => "events#show", as: "event"
   get "events/:id/edit" => "events#edit", as: "edit_event"
