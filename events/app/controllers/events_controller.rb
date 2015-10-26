b2a510e8427633347437448293368136898e9696
anne <richardson.ae@gmail.com>
2015-10-26 16:29:20 -0500

    private params and create form

--- events/app/controllers/events_controller.rb
+++ events/app/controllers/events_controller.rb
@@ -17,12 +17,27 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id]) 
-    event_params = params
-                  .require(:event)
-                  .permit(:name, :description, :location, :price, :starts_at)
-    @event.update(event_params)
+    @event.update(event_params) #comes from provate method below
     redirect_to event_path(@event) # or redirect_to @event
   end #update
+
+  def new
+    @event = Event.new 
+  end #new
+
+  def create    
+    @event = Event.new(event_params) # defines a new Event
+    @event.save # saves form info
+    redirect_to @event # redirects to the show page
+  end #create
+    
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+    end #event_params
+    
     
     
 end #EventsController
