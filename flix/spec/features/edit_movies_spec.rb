00cdbfcdce85941fde7fe8cf5fa9d32c5bed40ae
anne <richardson.ae@gmail.com>
2015-10-26 14:40:19 -0500

    added update spec and features

--- flix/spec/features/edit_movies_spec.rb
+++ flix/spec/features/edit_movies_spec.rb
@@ -2,7 +2,7 @@
 
 describe "Editing a movie" do
   
-  it "updates the movie and shows the movie's updated details" do
+  it "displays an update form for the movie" do
     movie = Movie.create(movie_attributes)
     
     visit movie_url(movie)
@@ -12,6 +12,21 @@ describe "Editing a movie" do
     expect(current_path).to eq(edit_movie_path(movie))
         
     expect(find_field('Title').value).to eq(movie.title)
+  end #it
+
+  it "takes user-updated data, saves it, and redirects to an updated show page" do
+    movie = Movie.create(movie_attributes)
+    
+    visit edit_movie_path(movie)
+
+    fill_in 'Title', with: "Updated Movie Title"
+
+    click_button 'Update Movie'
+
+    expect(current_path).to eq(movie_path(movie))
+
+    expect(page).to have_text('Updated Movie Title')
+    
   end #it
   
 end #describe
