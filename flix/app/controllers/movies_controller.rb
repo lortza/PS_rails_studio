670ea2763bcfd0da170f42674d2074b44ed30d5c
anne <richardson.ae@gmail.com>
2015-10-26 17:07:46 -0500

    added new and create methods to handle new data

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -13,12 +13,25 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id]) 
-    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     @movie.update(movie_params)
     redirect_to @movie
-
   end #update
+
+  def new # only need to create a new movie here
+    @movie = Movie.new(params[:movie])
+  end #new
+
+  def create # need to save this movie into the DB
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end #create
     
+  private
+    def movie_params
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    end #movie_params
+          
     
     
 end #MoviesController
