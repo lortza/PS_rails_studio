0110a6f19eeea5198a42f54377c9fa10a0fed0e3
anne <richardson.ae@gmail.com>
2015-11-02 13:29:09 -0600

    added validations

--- events/app/models/event.rb
+++ events/app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ActiveRecord::Base
+
+  validates :name, :location, presence: true
+  validates :description, length: {minimum: 25}
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  validates :capacity, numericality: {only_integer: true, greater_than: 0}
+  validates :image_file_name, allow_blank: true, format: {
+    with: /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  } # the "with" is a ruby regular expression
+
+
   def free?
     price.zero? || price.blank?
   end #free?
@@ -8,3 +19,13 @@ class Event < ActiveRecord::Base
   end #self.upcoming
     
 end #Event
+
+    # t.string   "name"
+    # t.string   "location"
+    # t.decimal  "price"
+    # t.datetime "created_at",                  null: false
+    # t.datetime "updated_at",                  null: false
+    # t.datetime "starts_at"
+    # t.text     "description"
+    # t.string   "image_file_name"
+    # t.integer  "capacity",        default: 1
