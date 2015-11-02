bfd82d780bd83a25c71d533ab841c1e7382c78ee
anne <richardson.ae@gmail.com>
2015-11-02 14:33:39 -0600

    added validations and spec

--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -1,21 +1,46 @@
 class Movie < ActiveRecord::Base
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+      with: /\w+\.(gif|jpg|png)\z/i,
+      message: "only GIF, JPG, and PNG are permitted"
+    } #image_file_name
+  validates :rating, inclusion: { in: RATINGS }
+
+
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
-  end
+  end #self.released
   
   def self.hits
     where('total_gross >= 300000000').order('total_gross desc')
-  end
+  end #self.hits
   
   def self.flops
     where('total_gross < 10000000').order('total_gross asc')
-  end
+  end #self.flops
   
   def self.recently_added
     order('created_at desc').limit(3)
-  end
+  end #self.recently_added
   
   def flop?
     total_gross.blank? || total_gross < 50000000
-  end
-end
+  end #flop
+end #Movie
+
+# t.string   "title"
+# t.string   "rating"
+# t.decimal  "total_gross"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+# t.text     "description"
+# t.date     "released_on"
+# t.string   "cast"
+# t.string   "director"
+# t.string   "duration"
+# t.string   "image_file_name"
--- flix/spec/features/create_movie_spec.rb
+++ flix/spec/features/create_movie_spec.rb
@@ -10,7 +10,7 @@ describe "Creating a new movie" do
 
     fill_in "Title", with: "New Movie Title"
     fill_in "Description", with: "Superheroes saving the world from villains"
-    fill_in "Rating", with: "PG-13"
+    select "PG-13", from: "movie_rating"
     fill_in "Total gross", with: "75000000"
     select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
     fill_in "Cast", with: "The award-winning cast"
@@ -24,4 +24,5 @@ describe "Creating a new movie" do
 
     expect(page).to have_text('New Movie Title')
   end
-end
+end #describe
+
