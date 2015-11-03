12f8450e1cce2b33ffb6704b26f5f613d88425ac
anne <richardson.ae@gmail.com>
2015-11-03 14:25:51 -0600

    validations and specs for validations

--- crowdfund/app/models/project.rb
+++ crowdfund/app/models/project.rb
@@ -1,4 +1,18 @@
 class Project < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :description, length: {minimum: 25, maximum: 500}
+  validates :target_pledge_amount, numericality: { greater_than: 0 }
+  validates :image_file_name, allow_blank: true, format: {
+    with:    /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  } #image_file_name
+  validates :website, allow_blank: true, format: { 
+    with: /https?:\/\/[\S]+\b/i,
+    message: "Whoops! Looks like this isn't a valid web address. Make sure to include the 'http://' or 'https://' and an ending like '.com' and try again."
+   } #website
+
+
   def self.accepting_pledges
     where("pledging_ends_on >= ?", Time.now).order("pledging_ends_on asc")
   end
@@ -6,4 +20,14 @@ class Project < ActiveRecord::Base
   def pledging_expired?
     pledging_ends_on < Date.today
   end
-end
+end #Project
+
+# t.string   "name"
+# t.text     "description"
+# t.decimal  "target_pledge_amount"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+# t.string   "website"
+# t.date     "pledging_ends_on"
+# t.string   "team_members"
+# t.string   "image_file_name"
--- crowdfund/spec/models/project_spec.rb
+++ crowdfund/spec/models/project_spec.rb
@@ -12,4 +12,89 @@ describe "A project" do
 
     expect(project.pledging_expired?).to eq(false)
   end
-end
+
+  it "requires a name" do
+    project = Project.new(name: "")
+    project.valid?
+    expect(project.errors[:name].any?).to eq(true)
+  end #requires a name
+
+  it "requires a description" do
+    project = Project.new(description: "")
+    project.valid?
+    expect(project.errors[:description].any?).to eq(true)
+  end # requires a description
+
+  it "accepts a description up to 500 characters" do
+    project = Project.new(description: "X" * 501)
+    project.valid?
+    expect(project.errors[:description].any?).to eq(true)
+  end # accepts a description up to 500 characters
+
+  it "accepts a positive target pledge amount" do
+    project = Project.new(target_pledge_amount: 1)
+    project.valid?
+    expect(project.errors[:target_pledge_amount].any?).to eq(false)
+  end # accepts a positive target pledge amount
+
+  it "rejects a $0 target pledge amount" do
+    project = Project.new(target_pledge_amount: 0)
+    project.valid?
+    expect(project.errors[:target_pledge_amount].any?).to eq(true)
+  end # rejects a $0 target pledge amount
+
+  it "rejects a negative target pledge amount" do
+    project = Project.new(target_pledge_amount: -1)
+    project.valid?
+    expect(project.errors[:target_pledge_amount].any?).to eq(true)
+  end # rejects a negative target pledge amount
+
+  it "accepts properly formatted website URLs" do
+     sites = %w[http://example.com https://example]
+      sites.each do |site|
+        project = Project.new(website: site)
+        project.valid?
+        expect(project.errors[:website].any?).to eq(false)
+      end #sites.each do
+  end # accepts properly formatted website URLs
+
+  it "rejects improperly formatted website URLs" do
+    sites = %w[example.com http examplehttp]
+    sites.each do |site|
+      project = Project.new(website: site)
+      project.valid?
+      expect(project.errors[:website].any?).to eq(true)
+    end #sites.each do
+  end # rejects improperly formatted website URLs
+
+  it "accepts properly formatted image file names" do
+     file_names = %w[e.png project.png project.jpg project.gif MOVIE.GIF]
+      file_names.each do |file_name|
+        project = Project.new(image_file_name: file_name)
+        project.valid?
+        expect(project.errors[:image_file_name].any?).to eq(false)
+    end #file_names each do
+  end # accepts properly formatted image file names
+
+  it "rejects improperly formatted image file names" do
+    file_names = %w[project .jpg .png .gif project.pdf project.doc]
+    file_names.each do |file_name|
+      project = Project.new(image_file_name: file_name)
+      project.valid?
+      expect(project.errors[:image_file_name].any?).to eq(true)
+    end #file_names.each do
+  end #rejects improperly formatted image file names
+
+
+end #describe
+
+### Schema
+# t.string   "name"
+# t.text     "description"
+# t.decimal  "target_pledge_amount"
+# t.datetime "created_at"
+# t.datetime "updated_at"
+# t.string   "website"
+# t.date     "pledging_ends_on"
+# t.string   "team_members"
+# t.string   "image_file_name"
