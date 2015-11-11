ec6f5c4e5bf0cdbe686cf3dfbcd2c2f2db7fa6aa
anne <richardson.ae@gmail.com>
2015-11-11 11:29:52 -0600

    spec for delete method flash alert

--- crowdfund/spec/features/delete_project_spec.rb
+++ crowdfund/spec/features/delete_project_spec.rb
@@ -9,6 +9,13 @@ describe "Deleting a project" do
     click_link 'Delete'
 
     expect(current_path).to eq(projects_path)
-    expect(page).not_to have_text(project.name)
-  end
-end
+    expect(page).not_to have_text(project.description)
+  end #destroys
+
+  it "shows a confirmation upon deletion" do
+     project = Project.create(project_attributes)
+     visit project_path(project)
+     click_link "Delete"
+     expect(page).to have_text("deleted")
+  end # shows a confirmation upon deletion
+end #describe
