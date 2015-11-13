b93001a33b3e10e3b2cf41c36b38d5c72a8012a4
anne <richardson.ae@gmail.com>
2015-11-13 17:19:32 -0600

    show: pledge button controller: new and create methods

--- crowdfund/app/controllers/pledges_controller.rb
+++ crowdfund/app/controllers/pledges_controller.rb
@@ -1,8 +1,37 @@
 class PledgesController < ApplicationController
 
+  before_action :set_project
+
   def index
-    @project = Project.find(params[:project_id])
+    # @project = Project.find(params[:project_id])
     @pledges = @project.pledges
   end #index
+
+  def new #renders the form
+     @pledge = @project.pledges.new
+  end #new
+
+  def create
+    @pledge = @project.pledges.new(pledge_params)
+    if @pledge.save # this imploes true
+      redirect_to project_pledges_path(@project), notice: "Thanks for supporting #{@project.name}!"
+    else
+      render :new
+    end #if
+  end #create
+    
+
+private
+
+  def set_project
+    @project = Project.find(params[:project_id]) 
+  end #set_project
+
+  def pledge_params
+      params.require(:pledge).permit(:name, :city, :email, :comment, :amount) 
+  end #pledge_params
+    
+
+    
     
 end #PledgesController
