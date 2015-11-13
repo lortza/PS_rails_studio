91839d41933daa74485f0d1730d5dca5cb5fbe2a
anne <richardson.ae@gmail.com>
2015-11-13 17:14:12 -0600

    added spec for creating nested pledge

--- crowdfund/spec/features/create_pledge_spec.rb
+++ crowdfund/spec/features/create_pledge_spec.rb
@@ -1,34 +1,39 @@
-it "saves the pledge if it's valid" do
-  project = Project.create(project_attributes)
+describe "Creating a Pledge" do
 
-  visit project_url(project)
+   it "saves the pledge if it's valid" do
+    project = Project.create(project_attributes)
 
-  click_link 'Pledge!'
+    visit project_url(project)
 
-  expect(current_path).to eq(new_project_pledge_path(project))
+    click_link 'Pledge!'
 
-  fill_in "Name",  with: "Joe Smith"
-  fill_in "Email", with: "joe@gmail.com"
-  select  "100.0", :from => "pledge_amount"
-  fill_in "Comment", with: "Count me in!"
+    expect(current_path).to eq(new_project_pledge_path(project))
 
-  click_button 'Create Pledge'
+    fill_in "Name",  with: "Joe Smith"
+    fill_in "Email", with: "joe@gmail.com"
+    select  "100.0", :from => "pledge_amount"
+    fill_in "Comment", with: "Count me in!"
 
-  expect(current_path).to eq(project_pledges_path(project))
-
-  expect(page).to have_text("Thanks for pledging!")
-  expect(page).to have_text("Joe Smith")
-  expect(page).to have_text("$100.00")
-end
-
-it "does not save the pledge if it's invalid" do
-  project = Project.create(project_attributes)
-
-  visit new_project_pledge_url(project)
-
-  expect {
     click_button 'Create Pledge'
-  }.not_to change(Pledge, :count)
 
-  expect(page).to have_text('error')
-end
+    expect(current_path).to eq(project_pledges_path(project))
+
+    expect(page).to have_text("Thanks for pledging!")
+    expect(page).to have_text("Joe Smith")
+    expect(page).to have_text("$100.00")
+  end
+
+  it "does not save the pledge if it's invalid" do
+    project = Project.create(project_attributes)
+
+    visit new_project_pledge_url(project)
+
+    expect {
+      click_button 'Create Pledge'
+    }.not_to change(Pledge, :count)
+
+    expect(page).to have_text('error')
+  end
+end # Creating a Pledge
+
+
