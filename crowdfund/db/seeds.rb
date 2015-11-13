52e9cc12b4b88ae28b137ad1c8d462e30dc278a4
anne <richardson.ae@gmail.com>
2015-11-13 16:15:14 -0600

    seeds

--- crowdfund/db/seeds.rb
+++ crowdfund/db/seeds.rb
@@ -35,3 +35,54 @@ Project.create([
    image_file_name: "project-c.png"
  },
 ])
+
+Pledge.create([
+ {
+   name: "Nooch",
+   city: "Isla Mujeres",
+   email: "nooch@kitties.com",
+   amount: 200,
+   comment: "I love this idea. I am sl gld you're doing it.",
+   project_id: 1
+ },
+ {
+   name: "Zorro",
+   city: "New Orleans",
+   email: "pinkcape@gmail.com",
+   amount: 50,
+   comment: "I don't know much, but I know I want this. I don't know what it is but I want it.",
+   project_id: 1
+ },
+ {
+   name: "Da PC",
+   city: "NOLA", 
+   email: "blank@email.com",
+   amount: 300,
+   comment: "Normally I'd just sit on the chair, but this seems like a better solution.",
+   project_id: 2
+ },
+ {
+   name: "Da Blanc",
+   city: "Medellin",
+   email: "pewpewpew@yahoo.com",
+   amount: 100,
+   comment: "You don't even know me. Really.",
+   project_id: 2
+ },
+ {
+   name: "Willow",
+   city: "Skelleton Cushion, NOLA",
+   email: "meoooooooow@email.com",
+   amount: 100,
+   comment: "Meoooooooooow. Rarrrrrrrrreeeeoooooow.",
+   project_id: 3
+ },
+ {
+   name: "Yowlanda",
+   city: "Leon",
+   email: "underdabed@hiding.com",
+   amount: 50,
+   comment: "If this means I get more fish scraps, I'm in.",
+   project_id: 3
+ }
+])
