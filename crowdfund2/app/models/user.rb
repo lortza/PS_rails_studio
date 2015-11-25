b4747ca05c7d83e27cd02dfe057508e69ccd0658
anne <richardson.ae@gmail.com>
2015-11-24 21:24:28 -0600

    user model validations

--- crowdfund2/app/models/user.rb
+++ crowdfund2/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 6, allow_blank: true }
+  validates :name, presence: true
+  validates :email, presence: true,
+      format: /\A\S+@\S+\z/,
+      uniqueness: { case_sensitive: false }
 end
