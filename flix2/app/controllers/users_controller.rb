3f031e62c35edf2a1e6be9150afa37cab7ac837a
anne <richardson.ae@gmail.com>
2015-11-28 11:31:12 -0600

    user model, controller, and style sheets

--- flix2/app/controllers/users_controller.rb
+++ flix2/app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
-end
+
+  def index
+    @users = User.all 
+  end #index
+    
+end #UsersController
--- flix2/app/models/user.rb
+++ flix2/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :password, length: { minumum: 5, allow_blank: true }
+  validates :password, length: { minimum: 5, allow_blank: true }
   validates :name, presence: true
   validates :email, presence: true, 
     format: /\A\S+@\S+\z/,
