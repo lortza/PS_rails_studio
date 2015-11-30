cf9eed765dcb56260567c8cada879669b2f94e0e
anne <richardson.ae@gmail.com>
2015-11-29 19:37:30 -0600

    added spec and supporting files for new user signup

--- flix2/app/controllers/users_controller.rb
+++ flix2/app/controllers/users_controller.rb
@@ -1,12 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  #before_action :user_params
 
   def index
     @users = User.all 
   end #index
 
   def show
-    @user = User.find(params[:id]) 
+    #@user = User.find(params[:id]) 
   end #show
+
+  def new
+    @user = User.new 
+  end #new
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    if @user.save
+      redirect_to @user, notice: "Welcome #{@user.name}! Thanks for signing up!"
+    else
+      render :new
+    end 
+  end #create
     
     
+private
+  def set_user
+    @user = User.find(params[:id]) 
+  end #set_user
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end #user_params
+
 end #UsersController
--- flix2/config/routes.rb
+++ flix2/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  get "signup" => "users#new"
 
   root "movies#index"
+
   resources :movies do
     resources :reviews
   end
