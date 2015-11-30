6981da97aaec741c251c60ab8e90b1eba72d3d73
anne <richardson.ae@gmail.com>
2015-11-30 17:09:52 -0600

    Sessions: controller, new view, routes, sign in button

--- flix2/config/routes.rb
+++ flix2/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  resource :session
+
   resources :users
   get "signup" => "users#new"
 
   root "movies#index"
-
   resources :movies do
     resources :reviews
-  end
-end
+  end #movies
+  
+end #Routes
