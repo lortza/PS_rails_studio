fea792b35339eb5be0ca7d4a00d0680c5c3db8ba
anne <richardson.ae@gmail.com>
2015-11-29 18:54:21 -0600

    specs and supporting files for user show page

--- flix2/app/controllers/users_controller.rb
+++ flix2/app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all 
   end #index
+
+  def show
+    @user = User.find(params[:id]) 
+  end #show
+    
     
 end #UsersController
