4ca080ae6c33c4cb2e560acda9e608028ae0fa40
anne <richardson.ae@gmail.com>
2015-11-30 19:55:16 -0600

    Sessions controller Create method authentication

--- crowdfund2/app/controllers/sessions_controller.rb
+++ crowdfund2/app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
   def new
+    #renders the New view
   end #new
 
   def create
-     params[:email]
-     params[:password]
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end #if
   end #create
     
-end
+end #SessionsController
--- crowdfund2/app/models/user.rb
+++ crowdfund2/app/models/user.rb
@@ -12,5 +12,10 @@ class User < ActiveRecord::Base
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase) 
   end #gravatar_id
+
+  def self.authenticate(email_or_username, password)
+    user = User.find_by(email: email_or_username) || User.find_by(username: email_or_username)
+    user && user.authenticate(password)
+  end #authenticate
       
 end #User
