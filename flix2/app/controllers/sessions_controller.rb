f53ab2d006bb4b2e2af06ee96bae38c5988b3cdf
anne <richardson.ae@gmail.com>
2015-11-30 19:29:54 -0600

    Session create method

--- flix2/app/controllers/sessions_controller.rb
+++ flix2/app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
   def new
-  end
+  end #new
 
   def create
-  end
-end
+     user = User.find_by(email: params[:email])
+     if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       flash[:notice] = "Welcome back, #{user.name}!"
+       redirect_to user
+     else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+     end #if
+   end #create
+end #SessionsController
