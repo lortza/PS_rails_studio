e5c7bccfcf244b702e72f11d5454be5c6d0cbae4
anne <richardson.ae@gmail.com>
2015-12-01 16:33:50 -0600

    login toggle specs

--- flix2/spec/features/create_user_spec.rb
+++ flix2/spec/features/create_user_spec.rb
@@ -33,4 +33,11 @@ describe "Creating a new user" do
 
     expect(page).to have_text('error')
   end
+
+  it "saves the user and shows the user's profile page" do    
+    # existing code
+
+    expect(page).not_to have_link('Sign In')
+    expect(page).not_to have_link('Sign Up')
+  end
 end
--- flix2/spec/features/sign_in_spec.rb
+++ flix2/spec/features/sign_in_spec.rb
@@ -45,4 +45,40 @@ describe "Signing in" do
     expect(page).to have_text('Invalid')
   end
 
+  it "signs in the user if the email/password combination is valid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button 'Sign In'
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_link(user.name)
+    expect(page).not_to have_link('Sign In')
+    expect(page).not_to have_link('Sign Up')
+  end
+
+  it "does not sign in the user if the email/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "incorrect"
+
+    click_button 'Sign In'
+
+    expect(page).not_to have_link(user.name)
+    expect(page).to have_link('Sign In')
+    expect(page).to have_link('Sign Up')
+  end
+
 end #Signing in
