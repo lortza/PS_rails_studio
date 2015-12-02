b56afe0e3c64dd9c6f87851332bb720a5d2199e0
anne <richardson.ae@gmail.com>
2015-12-02 11:42:59 -0600

    moved current_user into Application Controller and created before_actions for user sign in

--- flix2/app/controllers/users_controller.rb
+++ flix2/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_signin, except: [:new, :create]
 
   def index
     @users = User.all 
