11f0224b3672d8947bb58c0c6ae1036da5a306a3
anne <richardson.ae@gmail.com>
2015-12-02 20:27:15 -0600

    added current_user_admin? to views to lock down buttons

--- flix2/app/controllers/application_controller.rb
+++ flix2/app/controllers/application_controller.rb
@@ -22,4 +22,17 @@ private
   end #correct_user?(user)
   helper_method :current_user?
 
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Whoops! Only Admins can access this page."
+    end #unless
+  end #require_admin
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end #current_user_admin?
+  helper_method :current_user_admin?
+    
+    
+
 end #ApplicationController
