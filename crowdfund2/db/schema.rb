b03dcb65f9486e9b9320337dc261a3f26400c3a1
anne <richardson.ae@gmail.com>
2015-12-03 13:29:30 -0600

    AddAdminToUsers migration

--- crowdfund2/db/schema.rb
+++ crowdfund2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130212034) do
+ActiveRecord::Schema.define(version: 20151203165421) do
 
   create_table "pledges", force: true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151130212034) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.boolean  "admin",           default: false
   end
 
 end
