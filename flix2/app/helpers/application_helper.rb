a3f20ea182161533644e2ef9d1cba251ef432d01
anne <richardson.ae@gmail.com>
2015-12-10 12:16:23 -0600

    added dynamic title tags and dynamic sidebar content

--- flix2/app/helpers/application_helper.rb
+++ flix2/app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
-    
+
+  def page_title
+    if content_for?(:title)
+      "Flix - #{content_for(:title)}"
+       #"Flix - " + content_for(:title)
+     else
+       "Flix"
+     end #if
+  end #page_title
+   
 end #ApplicationHelper
