73d4692e2f902bcb934cdd9858c4fde87fc00585
anne <richardson.ae@gmail.com>
2015-12-10 16:59:16 -0600

    inserted dynamic title tag

--- crowdfund2/app/helpers/application_helper.rb
+++ crowdfund2/app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
-module ApplicationHelper
+module ApplicationHelper 
+
+  def page_title
+    if content_for?(:title)
+       "Crowdfund - " + content_for(:title)
+     else
+       "Crowdfund"
+     end 
+  end #page_title
+    
     
 end #ApplicationHelper
