c3d2a7b675cbeb3b8f62e3b51ecfeeb74c6d77d9
anne <richardson.ae@gmail.com>
2015-12-09 18:51:29 -0600

    content_for tags in sidebar and title tag

--- events2/app/helpers/application_helper.rb
+++ events2/app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-    
+    def page_title
+      if content_for?(:title)
+        "EventApp - #{content_for(:title)}" 
+      else
+        "EventApp"
+      end #content_for
+    end #page_title
+      
 end #ApplicationHelper
