ff8b61c4e3b63acedf080b79fec81802ec9467c5
anne <richardson.ae@gmail.com>
2015-12-11 13:38:21 -0600

    added scopes to movies

--- flix2/app/models/movie.rb
+++ flix2/app/models/movie.rb
@@ -19,17 +19,17 @@ class Movie < ActiveRecord::Base
 
   validates :rating, inclusion: { in: RATINGS }
   
-  def self.released
-    where("released_on <= ?", Time.now).order(released_on: :desc)
-  end
+  scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
+
+  scope :hits, -> { released.where('total_gross >= 300000000').order(total_gross: :desc) }
   
-  def self.hits
-    where('total_gross >= 300000000').order(total_gross: :desc)
-  end
-  
-  def self.flops
-    where('total_gross < 50000000').order(total_gross: :asc)
-  end
+  scope :flops, -> { released.where('total_gross < 50000000').order(total_gross: :asc) }
+
+  scope :upcoming, -> { where("released_on > ?", Time.now).order(released_on: :asc) }
+
+  scope :rated, ->(rating) { released.where(rating: rating) }
+
+  scope :recent, ->(max=5) { released.limit(max) }
   
   def flop?
     total_gross.blank? || total_gross < 50000000
