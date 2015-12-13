b20e8da0051113bfa71ebdae98e63d82299cf990
anne <richardson.ae@gmail.com>
2015-12-13 14:17:10 -0600

    added custom nav links and active behavior for scope urls

--- flix2/app/helpers/application_helper.rb
+++ flix2/app/helpers/application_helper.rb
@@ -8,5 +8,11 @@ module ApplicationHelper
        "Flix"
      end #if
   end #page_title
+
+  def nav_link_to(text, url)
+    classes = ['button']
+    classes << 'active' if current_page?(url)
+    link_to(text, url, class: classes.join(' '))
+  end #nav_link_to
    
 end #ApplicationHelper
