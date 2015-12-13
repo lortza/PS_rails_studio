a8a867195364f4d20f4d89c0b18a38e886ae5a45
anne <richardson.ae@gmail.com>
2015-12-13 14:54:34 -0600

    added links to scoped urls

--- crowdfund2/app/helpers/application_helper.rb
+++ crowdfund2/app/helpers/application_helper.rb
@@ -7,6 +7,13 @@ module ApplicationHelper
        "Crowdfund"
      end 
   end #page_title
+
+  def nav_link_to(text, url)
+    classes = ['button']
+    classes << 'active' if current_page?(url)
+    link_to(text, url, class: classes.join(' '))
+  end #nav_link_to
+    
     
     
 end #ApplicationHelper
