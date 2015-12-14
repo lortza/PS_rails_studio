1e9621234fe186723e64d7f6b3683dfed13ab7de
anne <richardson.ae@gmail.com>
2015-12-14 15:34:24 -0600

    corrected set_movie method

--- flix2/app/controllers/favorites_controller.rb
+++ flix2/app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
   before_action :set_movie
 
   def create
-    @movie = Movie.find(params[:movie_id])
+    #@movie = Movie.find(params[:movie_id])
     @movie.favorites.create!(user: current_user)
     redirect_to @movie, notice: "Faved!"
   end #create
 
   def destroy
-    @movie = Movie.find(params[:movie_id])
+    #@movie = Movie.find(params[:movie_id])
     favorite = current_user.favorites.find(params[:id])
     favorite.destroy
     redirect_to @movie, notice: "OH SNAP! Unfaved!"
